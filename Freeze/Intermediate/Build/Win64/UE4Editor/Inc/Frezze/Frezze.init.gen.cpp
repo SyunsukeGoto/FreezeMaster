a507654d99ba4797f96b540e92f67aae1bec82e8
さとうももや <goodragon2158@gmail.com>
2019-09-05 14:00:16 +0900

    水を生成するブロックとそれのトリガーとなるブロックの作成しました

--- Freeze/Intermediate/Build/Win64/UE4Editor/Inc/Frezze/Frezze.init.gen.cpp
+++ Freeze/Intermediate/Build/Win64/UE4Editor/Inc/Frezze/Frezze.init.gen.cpp
@@ -21,7 +21,7 @@ void EmptyLinkFunctionForGeneratedCodeFrezze_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0xAFF356C3,
+				0x614F06C9,
 				0xCF0ED355,
 				METADATA_PARAMS(nullptr, 0)
 			};
